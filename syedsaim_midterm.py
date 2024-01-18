9e50750c29537cc40238d12b2d80e90c00b9b60c
SyedSaimWarsi <143235829+SyedSaimWarsi@users.noreply.github.com>
2024-01-18 09:08:06 -0800

    Add files via upload

---
+++ syedsaim_midterm.py
@@ -0,0 +1,60 @@
+#SYED SAIM ALI WARSI
+# MID TERM PAPER 
+
+#Question Number 1
+#Write a Python program to do arithmetical operations addition and division.
+
+a=print(input("Enter the first number for addition:"))
+b=print(input("Enter the Second number for addition:"))
+sum = a+b
+print("sum :",a, "+" ,b)
+
+
+a=print(input("Enter the first number for devision:"))
+b=print(input("Enter the Second number for devision:"))
+sum = a+b
+print("sum :",a, "/" ,b)
+
+
+#Write a Python program to find the area of a triangle.
+
+base=print(input("Enter the length of the base of the triangle: "))
+len=print(input("Enter the length of the base of the triangle: "))
+area = 1/2*(len*base)
+print("The area of the triangle is :"area)
+
+#Write a Python program to convert Celsius to Fahrenheit.
+cel=print(input("Enter the temperature in celcius:"))
+convrt = (cel*(9/5))+ 32
+print(cel,"degrees is equal to ",convrt "degree fahrenhiet")
+
+#Question Number 4
+#Write a Python program that return all datatypes that we discussed in the class.
+
+num=1
+print("the data type of ",a"is",type(a))
+
+fl=233.3
+print("the data type of ",a"is",type(fl))
+
+let="asbs"
+print("the data type of ",a"is",type(let))
+
+bl= 5==5
+print("the data type of ",a"is",type(bl))
+
+lst=[1,2,3,3,4,4]
+print("the data type of ",a"is",type(lst))
+
+sets={1,2,3,4,5,6}
+print("the data type of ",a"is",type(sets))
+
+tup=(1,2,3,44,5,5)
+print("the data type of ",a"is",type(tup))
+
+
+
+
+
+
+
